468183cc81be4649efeebcfcd76220615c27151e
Vincent Sgherzi <sgherzivincent@gmail.com>
2024-12-21 02:59:33 -0800

    working solution day 5

--- five/src/main.rs
+++ five/src/main.rs
@@ -1,62 +1,117 @@
-use std::{collections::HashMap, fs};
+use std::fs;
 
 fn main() {
-    let lines = fs::read_to_string("input.txt").expect("could not find input");
-    let mut lines = lines.lines().collect();
+    let input = fs::read_to_string("input.txt")
+        .expect("could not find input");
 
-    let results = order_pages(&lines);
+    let words: Vec<&str> = input
+        .split("\n\n")
+        .collect();
+    
+    let rules: Vec<(u32, u32)> = words[0]
+        .split("\n")
+        .map(|x| {
+            let temp: Vec<u32> = x
+                .split("|")
+                .map(|y| y.parse::<u32>().unwrap())
+                .collect();
+            (temp[0], temp[1])
+        })
+        .collect();
 
-    let updates = lines.split_off(results.1 + 1);
-    let rules = results.0;
+    let mut reports: Vec<Vec<u32>> = words[1]
+        .split("\n")
+        .map(|x| {
+            x.split(",").map(|x| x.parse::<u32>().unwrap())
+            .collect()
+        })
+        .collect();
+
+   
+    // let total = part_one(reports, rules);
+    // println!("total is {total:?}");
+
+    let total = part_two(&mut reports, rules);
+    println!("total is {total:?}");
+}
+
+fn check_rules(rules: Vec<(u32, u32)>, report: &mut Vec<u32>) -> (bool, Vec<u32>){
+
+    let did_pass = true;
+
+    for rule in rules.iter(){
+
+        let comes_before_index = match report.iter().position(|&r| r == rule.0){
+            Some(comes_before_index) => comes_before_index,
+            None => continue,
+        };
+
+        let comes_after_index = match report.iter().position(|&r| r == rule.1){
+            Some(comes_after_index) => comes_after_index,
+            None => continue,
+        };
+
+        if comes_before_index > comes_after_index {
+            report.swap(comes_before_index,comes_after_index);
+            return (false, report.to_vec())
+        }
+    }
+
+    (did_pass, report.to_vec())
+}
+
+fn part_two(reports: &mut Vec<Vec<u32>>, rules: Vec<(u32, u32)>) -> u32{
 
     let mut total = 0;
-    for update in updates {
-        let update: Vec<usize> = update
-            .split(',')
-            .map(|x| x.parse::<usize>().unwrap())
-            .collect();
-        if check_update(&update, &rules) {
-            let half = update.get(update.len() / 2).unwrap();
-            total += half;
+
+    for report in reports {
+
+        let mut counter = 0;
+        let mut rule_check = check_rules(rules.clone(), report);
+
+        while rule_check.0 == false {
+            counter += 1;
+            rule_check = check_rules(rules.clone(), &mut rule_check.1);
+        }
+
+        if counter > 0{
+            total += rule_check.1.get(rule_check.1.len() / 2).unwrap();
         }
     }
 
-    println!("the total is {total:?}");
+    total
 }
 
-fn check_update(update: &Vec<usize>, rules: &HashMap<usize, usize>) -> bool {
-    let mut update_map = HashMap::new();
 
-    for (index, entry) in update.iter().enumerate() {
-        update_map.insert(entry, index);
-    }
+fn part_one(reports: Vec<Vec<u32>>, rules: Vec<(u32, u32)>) -> u32{
 
-    for rule in rules {
-        if let Some(key_index) = update_map.get(rule.0) {
-            if let Some(value_index) = update_map.get(rule.1) {
-                if value_index < key_index {
-                    return false;
-                }
+    let mut total = 0;
+
+    for report in reports {
+
+        let mut is_good = true;
+        for rule in rules.iter(){
+
+
+            let comes_before_index = match report.iter().position(|&r| r == rule.0){
+                Some(comes_before_index) => comes_before_index,
+                None => continue,
+            };
+
+            let comes_after_index = match report.iter().position(|&r| r == rule.1){
+                Some(comes_after_index) => comes_after_index,
+                None => continue,
+            };
+
+            if comes_before_index > comes_after_index {
+                is_good = false;
             }
+        };
+
+        if is_good {
+            total += report.get(report.len() / 2).unwrap();
         }
     }
-    true
-}
 
-fn order_pages(lines: &Vec<&str>) -> (HashMap<usize, usize>, usize) {
-    let mut order = HashMap::new();
-    for (count, line) in lines.iter().enumerate() {
-        let line = *line;
-
-        if line == "" {
-            return (order, count);
-        }
-
-        let rule: Vec<&str> = line.split("|").collect();
-
-        let key: usize = rule.get(0).unwrap().parse().unwrap();
-        let value: usize = rule.get(1).unwrap().parse().unwrap();
-        order.insert(key, value);
-    }
-    (order, 0)
+    total
 }
---
+++ five/target/debug/deps/five-d29c448c5846b821.d
@@ -0,0 +1,9 @@
+/Users/vincentsgherzi/coding/aoc2024/five/target/debug/deps/libfive-d29c448c5846b821.rmeta: src/main.rs Cargo.toml
+
+/Users/vincentsgherzi/coding/aoc2024/five/target/debug/deps/five-d29c448c5846b821.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
